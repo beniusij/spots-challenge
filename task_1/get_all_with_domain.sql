cab7b21671cf71688bce1657a7c56966d8b4c494
Juozas Beniusis <jb@bluetel.co.uk>
2022-11-15 18:49:23 +0000

    Finish 1.1

--- task_1/get_all_with_domain.sql
+++ task_1/get_all_with_domain.sql
@@ -1,14 +1,1 @@
-CREATE OR REPLACE FUNCTION get_all_with_domain()
-    RETURNS SETOF "MY_TABLE" AS
-$$
-DECLARE
-    spots "MY_TABLE";
-BEGIN
-    FOR spots IN
-        SELECT * FROM "MY_TABLE" WHERE array_length(regexp_matches(website, ''), 1) > 0
-    LOOP
-        RETURN NEXT spots;
-    END LOOP;
-    RETURN;
-END;
-$$ LANGUAGE plpgsql;
+SELECT * FROM "MY_TABLE" WHERE website ~ '(:?www.)?((?:[a-z0-9-]+\.)+(?:[a-z]+))(?:\/)?';
